a58c610984fb1d08a55840b2e045333bb326bf18
Shawn Taylor <shawntaylor@weecology.org>
2017-08-31 18:31:03 -0400

    add in error metric calculation
    These results are in raster format, so it's easier done here.

--- cost_loss_analysis.R
+++ cost_loss_analysis.R
@@ -29,9 +29,29 @@ aggregate_temporally = function(x, fact=2, fun=max, keep_original_layer_count=TR
   return(out)  
 }
 
-resample_temporally = function(x, y){
-  output_layers = nlayers(y)
+#Various scoring metrics
+precision = function(obs, pred){
+  correctly_predicted = obs==pred
+  tp = sum(correctly_predicted & pred==1)
+  fp = sum((!correctly_predicted) & pred==1)
+  return( tp / (tp + fp))
 }
+
+recall = function(obs, pred){
+  correctly_predicted = obs==pred
+  tp = sum(correctly_predicted & pred==1)
+  fn = sum((!correctly_predicted) & pred==0)
+  return( tp / (tp + fn))
+}
+
+f1_score = function(obs, pred){
+  prec = precision(obs, pred)
+  rec = recall(obs, pred)
+  return( (2 * prec * rec) / sum(prec, rec))
+}
+
+
+#######################################################################
 
 spatial_scales = c(1,2,5)
 temporal_scales = c(1,2,3,6)
@@ -49,7 +69,7 @@ observation_rasters = observation_rasters[!grepl('xml', observation_rasters)]
 forecasts = raster::stack(forecast_rasters)
 observations = raster::stack(observation_rasters)
 
-#Rows * columns * years
+#Total 500x500m cells. Rows * columns * years
 total_cells = prod(dim(forecasts))
 
 #Convert to yes or no for class 10
@@ -57,6 +77,44 @@ is_class_10 = function(x){(x==10)*1}
 forecasts = calc(forecasts, is_class_10)
 observations = calc(observations, is_class_10)
 
+
+#############################################################################
+#Get csv of predictions/observations at all scales
+
+scaled_data = data.frame()
+for(this_spatial_scale in spatial_scales){
+  for(this_temporal_scale in temporal_scales){
+    if(this_spatial_scale > 1){
+      forecasts_upscaled = aggregate(forecasts, fact=this_spatial_scale, fun=max)
+      observations_upscaled = aggregate(observations, fact=this_spatial_scale, fun=max)
+      #Resample so it retains the original dimensions and cell numbers
+    } else {
+      forecasts_upscaled = forecasts
+      observations_upscaled = observations
+    }
+    
+    if(this_temporal_scale > 1){
+      forecasts_upscaled    = aggregate_temporally(forecasts_upscaled, fact = this_temporal_scale, keep_original_layer_count = FALSE)
+      observations_upscaled = aggregate_temporally(observations_upscaled, fact = this_temporal_scale, keep_original_layer_count = FALSE)
+    } 
+    
+    this_scale_data = data.frame(observed = as.vector(observations_upscaled),
+                                 predicted = as.vector(forecasts_upscaled))
+    
+    this_scale_data$spatial_scale = this_spatial_scale
+    this_scale_data$temporal_scale = this_temporal_scale
+    this_scale_data$cell_id = 1:nrow(this_scale_data)
+    
+    scaled_data = scaled_data %>%
+      bind_rows(this_scale_data)
+    
+  }
+}
+
+
+
+###############################################################################
+#Calculate cost/loss model curve
 r=data.frame()
 for(loss_cost in possible_loss_costs){
   smallest_grain_perfect = (sum(as.vector(observations)) * treatment_cost) / total_cells
@@ -67,7 +125,7 @@ for(loss_cost in possible_loss_costs){
     for(this_temporal_scale in temporal_scales){
       if(this_spatial_scale > 1){
         forecasts_upscaled = aggregate(forecasts, fact=this_spatial_scale, fun=max)
-        #Resample so it retains the origin dimensions and cell numbers
+        #Resample so it retains the original dimensions and cell numbers
         forecasts_upscaled = resample(forecasts_upscaled, forecasts, method='ngb')
       } else {
         forecasts_upscaled = forecasts
@@ -97,7 +155,3 @@ for(loss_cost in possible_loss_costs){
 r$value = with(r, (expense_max - expense_forecast) / (expense_max - expense_perfect))
 r$this_scale = with(r, paste0('space_',spatial_scale,'_time_',temporal_scale))
 
-ggplot(r, aes(x=a, y=value, color=as.factor(spatial_scale), group=as.factor(this_scale))) + 
-  geom_line(aes(linetype=as.factor(temporal_scale))) +
-  ylim(0,1) 
-  facet_grid(~temporal_scale)
